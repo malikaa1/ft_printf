0838f404e493c9cfea9693367ffdd4eadeee0a46
mrahmani <mrahmani@lenovo-pc.localdomain>
2020-12-28 12:03:49 +0100

    norminette

--- src/ft_char_spec.c
+++ src/ft_char_spec.c
@@ -1,38 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_char_spec.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrahmani <mrahmani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/27 13:43:08 by mrahmani          #+#    #+#             */
+/*   Updated: 2020/12/27 13:56:15 by mrahmani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
-int ft_print_c_speci(char flag, char c, int width)
-{
-    int i;
-    int count;
 
-    count = 0;
-    i = width - 1;
-    if (flag == '-' || (flag == ' ' && width == 0))
-        count += write_char(c);
-    while (i > 0)
-    {
-        count += write_char(' ');
-        i--;
-    }
-    if (flag == ' ' && width != 0)
-        count += write_char(c);
-    return (count);
+int	ft_print_c_speci(char flag, char c, int width)
+{
+	int		i;
+	int		count;
+
+	count = 0;
+	i = width - 1;
+	if (flag == '-' || (flag == ' ' && width == 0))
+		count += write_char(c);
+	while (i > 0)
+	{
+		count += write_char(' ');
+		i--;
+	}
+	if (flag == ' ' && width != 0)
+		count += write_char(c);
+	return (count);
 }
-int output_c_specifier(va_list *parms_arry, format_parser *parser)
-{
-    char c;
-    int width;
 
-    if (parser->is_dynamic_wdith == 1)
-    {
-        width = va_arg(*parms_arry, int);
-        if (width < 0)
-        {
-            width = width * -1;
-            parser->flag = '-';
-        }
-    }
-    else
-        width = parser->width;
-    c = va_arg(*parms_arry, int);
-    return (ft_print_c_speci(parser->flag, c, width));
+int	output_c_specifier(va_list *parms_arry, format_parser *parser)
+{
+	char	c;
+	int		width;
+
+	if (parser->is_dynamic_wdith == 1)
+	{
+		width = va_arg(*parms_arry, int);
+		if (width < 0)
+		{	
+			width = width * -1;
+			parser->flag = '-';
+		}
+	}
+	else
+		width = parser->width;
+	c = va_arg(*parms_arry, int);
+	return (ft_print_c_speci(parser->flag, c, width));
 }
--- src/ft_pointer_spec.c
+++ src/ft_pointer_spec.c
@@ -1,70 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pointer_spec.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrahmani </var/mail/mrahmani>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/27 16:47:30 by mrahmani          #+#    #+#             */
+/*   Updated: 2020/12/27 17:32:32 by mrahmani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int write_flag_p(int size)
+int	write_flag_p(int size)
 {
-    int count;
+	int		count;
 
-    count = 0;
-    while (size > 0)
-    {
-        ft_putchar(' ');
-        count++;
-        size--;
-    }
-    return count;
+	count = 0;
+	while (size > 0)
+	{
+		ft_putchar(' ');
+		count++;
+		size--;
+	}
+	return (count);
 }
 
-int output_pointer_flag(long long int ptr, int width, int precision, char flag)
+int	output_pointer_flag(long long int ptr, int width, int precision, char flag)
 {
-    int length;
-    int count;
+	int		length;
+	int		count;
 
-    count = 0;
-    length = ft_strlen(ft_itoa(ptr, "0123456789abcdef"));
-    width = width > length ? (width - precision - length - 2) : 0;
-    if (flag == ' ' || flag == '0')
-    {
-        count += write_flag_p(width);
-        count += write_string("0x");
-        while ((precision - length) > 0)
-        {
-            count += write_char('0');
-            precision--;
-        }
-        count += write_string(ft_itoa(ptr, "0123456789abcdef"));
-    }
-    if (flag == '-')
-    {
-        count += write_string("0x");
-        count += write_string(ft_itoa(ptr, "0123456789abcdef"));
-        count += write_flag_p(width);
-    }
-    return (count);
+	count = 0;
+	length = ft_strlen(ft_itoa(ptr, "0123456789abcdef"));
+	if (width > lenght)
+		width = width - precision - length - 2;
+	else
+		width = 0;
+	if (flag == ' ' || flag == '0')
+	{
+		count += write_flag_p(width);
+		count += write_string("0x");
+		while ((precision - length) > 0)
+		{	
+			count += write_char('0');
+			precision--;
+		}
+		count += write_string(ft_itoa(ptr, "0123456789abcdef"));
+	}
+	if (flag == '-')
+	{
+		count += write_string("0x");
+		count += write_string(ft_itoa(ptr, "0123456789abcdef"));
+		count += write_flag_p(width);
+	}
+	return (count);
 }
 
-int output_p_specifier(va_list *parms_arry, format_parser *parser)
+int	output_p_specifier(va_list *parms_arry, format_parser *parser)
 {
-    long long int ptr;
-    int width;
-    int length;
-    int precision;
+	long long int		ptr;
+	int		width;
+	int		length;
+	int		precision;
 
-    if (parser->is_dynamic_wdith == 1)
-    {
-        width = va_arg(*parms_arry, int);
-        if (width < 0)
-        {
-            width = width * -1;
-            parser->flag = '-';
-        }
-    }
-    else
-        width = parser->width;
-    ptr = va_arg(*parms_arry, long long int);
-    length = ft_strlen(ft_itoa(ptr, "0123456789abcdef"));
-    precision = parser->precision < length || parser->precision == -1 ? 0 : parser->precision;
-    if (ptr == 0)
-        return write_string("(nil)");
-    else
-        return output_pointer_flag(ptr, width, precision, parser->flag);
-}
+	if (parser->is_dynamic_wdith == 1)
+	{
+		width = va_arg(*parms_arry, int);
+		if (width < 0)
+		{
+			width = width * -1;
+			parser->flag = '-';
+		}
+	}
+	else
+		width = parser->width;
+	ptr = va_arg(*parms_arry, long long int);
+	length = ft_strlen(ft_itoa(ptr, "0123456789abcdef"));
+	if (parser->precision < length || parser->precision == -1 )
+		precision = 0;
+	else
+		parser->precision;
+	if (ptr == 0)
+		return (write_string("(nil)"));
+	else
+		return (output_pointer_flag(ptr, width, precision, parser->flag));
